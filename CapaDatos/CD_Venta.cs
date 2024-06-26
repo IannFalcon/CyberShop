2ab55a989aae98f8045acb0af706d2ba512c81f3
Iann <noseassapo@gmail.com>
2024-06-26 18:24:17 -0500

    JsonResult para listar ventas agregado

---
+++ CapaDatos/CD_Venta.cs
@@ -0,0 +1,58 @@
+﻿using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Venta
+    {
+        public List<Reporte> ListarVentas(string fechaini, string fechafin, string idtransaccion)
+        {
+            List<Reporte> listado = new List<Reporte>();
+
+            try
+            {
+                using (SqlConnection cnx = new SqlConnection(Conexion.con))
+                {
+                    SqlCommand cmd = new SqlCommand("LISTAR_VENTAS", cnx);
+                    cmd.Parameters.AddWithValue("FechaInicio", fechaini);
+                    cmd.Parameters.AddWithValue("FechaFin", fechafin);
+                    cmd.Parameters.AddWithValue("IdTransaccion", idtransaccion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cnx.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            listado.Add(new Reporte()
+                            {
+                                FechaVenta = dr[0].ToString(),
+                                NombreCliente = dr[1].ToString(),
+                                Producto = dr[2].ToString(),
+                                Precio = Convert.ToDecimal(dr[3], new CultureInfo("es-PE")),
+                                Cantidad = Convert.ToInt32(dr[4]),
+                                Total = Convert.ToDecimal(dr[5], new CultureInfo("es-PE")),
+                                IdTransaccion = dr[6].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                listado = new List<Reporte>();
+            }
+
+            return listado;
+
+        }
+    }
+}
---
+++ CapaNegocio/CN_Venta.cs
@@ -0,0 +1,20 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CN_Venta
+    {
+        private CD_Venta objCapaDatos = new CD_Venta();
+
+        public List<Reporte> Ventas(string fechaini, string fechafin, string idtransaccion)
+        {
+            return objCapaDatos.ListarVentas(fechaini, fechafin, idtransaccion);
+        }
+    }
+}
