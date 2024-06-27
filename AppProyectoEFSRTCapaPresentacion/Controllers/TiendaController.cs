70e9cc94f7890c9771d867d2646dd704fa787337
Iann <noseassapo@gmail.com>
2024-06-27 16:03:08 -0500

    Login y avance en el index del modulo clientes

--- AppProyectoEFSRTCapaPresentacion/Controllers/TiendaController.cs
+++ AppProyectoEFSRTCapaPresentacion/Controllers/TiendaController.cs
@@ -2,6 +2,7 @@
 using CapaNegocio;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,6 +16,62 @@ namespace AppProyectoEFSRTCapaPresentacion.Controllers
         {
             return View();
         }
+
+        #region INDEX_CLIENTE
+
+        [HttpGet]
+        public JsonResult ListaCategorias()
+        {
+            List<Categoria> lista = new CN_Categoria().Listar();
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult ListaMarcas() 
+        {
+            List<Marca> lista = new CN_Marca().Listar();
+            return Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult ListarProductos(int idcategoria, int idmarca) 
+        {
+            List<Producto> lista = new List<Producto>();
+
+            bool conversion;
+
+            lista = new CN_Productos().Listar().Select(p => new Producto()
+            {
+
+                IdProducto = p.IdProducto,
+                Nombre = p.Nombre,
+                Descripcion = p.Descripcion,
+                objCategoria = p.objCategoria,
+                objMarca = p.objMarca,
+                Precio = p.Precio,
+                Stock = p.Stock,
+
+                RutaImagen = p.RutaImagen,
+                Base64 = CN_Recursos.ConvertirBase64(Path.Combine(p.RutaImagen, p.NombreImagen), out conversion),
+                Extension = Path.GetExtension(p.NombreImagen),
+
+                Activo = p.Activo
+
+            }).Where(p => 
+
+                p.objCategoria.IdCategoria == (idcategoria == 0 ? p.objCategoria.IdCategoria : idcategoria) &&
+                p.objMarca.IdMarca == (idmarca == 0 ? p.objMarca.IdMarca : idmarca) &&
+                p.Stock > 0 && p.Activo == true
+
+            ).ToList();
+
+            var jsonresult = Json(new { data = lista }, JsonRequestBehavior.AllowGet);
+            jsonresult.MaxJsonLength = int.MaxValue;
+
+            return jsonresult;
+        }
+
+        #endregion
 
         #region CARRITO
 
